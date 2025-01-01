7c831f287368596e4b503f565bc400bd02fb5b42
Vietanh-Vu <vuvietanh993@gmail.com>
2025-01-01 15:17:19 +0700

    fix conflict

--- AIMS/src/main/java/isd/aims/main/entity/payment/PaymentType.java
+++ AIMS/src/main/java/isd/aims/main/entity/payment/PaymentType.java
@@ -3,10 +3,16 @@ package isd.aims.main.entity.payment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public enum PaymentType {
     VNPay("VNPay");
 
     private final String paymentType;
+    public static List<PaymentType> getAllPaymentTypes() {
+        return Arrays.asList(PaymentType.values());
+    }
 }
